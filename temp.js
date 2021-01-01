9f77d422f86842316035ab4007ab4fd637b8694c
stravo1 <stravo1@pop-os.localdomain>
2021-01-01 10:50:34 +0530

    Minor and Major Bug Fixes
    Quite stable, needs further testing to find more bugs

--- temp.js
+++ temp.js
@@ -1,6 +1,6 @@
 
 // define a new console
-var console=(function(oldCons){
+var consolen=(function(oldCons){ //*n
     return {
         log: function(text){
             oldCons.log(text);
@@ -20,7 +20,7 @@ var console=(function(oldCons){
         },
        old: {
          log: (text) => {
-           oldCons.log(text);
+           undefined
          },
          info: (text) => {
            oldCons.info(text);
@@ -30,55 +30,39 @@ var console=(function(oldCons){
 }(window.console));
 
 //Then redefine the old console
-window.console = console;
+window.console = consolen;
 
+
+drk = 0; // initially in day mode
 
 //ObtainTxtOnInputField
 function txt() {
     return document.getElementsByClassName("txtinp")[0].value;
 }
+// Get the header
+var header = document.getElementById("header");
 
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
 
-//adds script, prints to 'console'
-const scrpt_exec = () => {
-   document.getElementById('console').innerHTML+="<span class='span'>> "+txt()+"</span><br>"; //prints the input to console
-   //dynamically adding span doesnt effect if attributes rare changed later COS YOU WERE FXCKING USIND 1ID FORALL SPANS
-    var newScript = document.createElement('script');
-    newScript.setAttribute("class","nwscrpt");
-    var inlineScript = document.createTextNode(txt());
-    var div = document.getElementById('scrptHere');
-    newScript.appendChild(inlineScript);
-    div.appendChild(newScript); //adds script
-}
-const cnsole_final = () => { //click action
-    try {
-        //window.console = console.old;
-        let tmp = eval(txt()) ; //for args ;ike just  the name of a var or '2+3' etc
-        //eval has to be used bwith old console.log()
-        window.console = console;
-        if (tmp == undefined) {
-            scrpt_exec();
-        } else {
-            document.getElementById('console').innerHTML+="<span class='span'>> "+txt()+"</span><br>";
-            console.log(tmp)
-        }
-    } catch(err) {
-        document.getElementById('console').innerHTML+="<span class='span'>> "+txt()+"</span><br>";
-        console.log(err);
-    }
-        document.getElementById('txtinp').value= ""; //clear the cmnd line
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function headerFixed() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
 }
 
-document.getElementsByClassName("bttn")[0].addEventListener("click", function(){
-    cnsole_final()
-})
-document.getElementsByClassName("txtinp")[0].addEventListener("keyup", function(event) { //foe Enter Button
-    if (event.keyCode === 13) {
-     event.preventDefault();
-     document.getElementsByClassName("bttn")[0].click();
-    }
-})
-drk = 0; // initially in day mode
+
+
+const automate0 = (arg) => { //add spans with color depending upon drk state (was a bug)
+    var color = ['#2a62c9','#aec6f5']
+    document.getElementById('console').innerHTML+="<span style='color : "+color[arg]+"'>> "+txt()+"</span><br>";
+}
+
+
+//automate changing the classes to 'l' or 'd'
 const automate1 = (arg) => {
     var id = ['h1','dark','readonly','txtinp','bttn'];
     var state = ['l','d'];
@@ -87,17 +71,52 @@ const automate1 = (arg) => {
         }
 }
 const automate2 = (arg) => {
-    var id = ['console',]
     var state = [['whitesmoke','#aec6f5'],['black','#2a62c9']];
-    for (let i = 0; i < id.length; i++){
-        document.getElementById(id[i]).style.setProperty('color',state[arg][i]);
-        document.body.setAttribute('id','body'+ (arg===0 ? 'd':'l'));
-    }
+
+    document.getElementById('console').style.setProperty('color',state[arg][0]);
+    document.body.setAttribute('id','body'+ (arg===0 ? 'd':'l'));
+
     var id = document.getElementsByTagName('span')
     for (var i = 0; i < id.length; i++){
         id[i].style.setProperty('color',state[arg][1])
     }
 }
+
+//adds script, prints to 'console'
+const scrpt_exec = () => {
+    //window.consolen = console;
+   automate0(drk); //prints the input to console
+   //dynamically adding span doesnt effect if attributes rare changed later COS YOU WERE FXCKING USIND 1ID FORALL SPANS
+    var newScript = document.createElement('script');
+    newScript.setAttribute("class","nwscrpt");
+    var inlineScript = document.createTextNode(txt());
+    var div = document.getElementById('scrptHere');
+    newScript.appendChild(inlineScript);
+    div.appendChild(newScript); //adds script
+}
+
+
+
+const cnsole_final = () => { //exec bttn click action
+    try {
+        window.console = consolen.old;
+        let tmp = eval(txt()) ; //for args ;ike just  the name of a var or '2+3' etc
+        //eval has to be used bwith old console.log()
+        window.console = consolen;
+        if (tmp == undefined) {
+            scrpt_exec();
+        } else {
+            automate0(drk)
+            console.log(tmp)
+        }
+    } catch(err) {
+        window.console = consolen;
+        automate0(drk)
+        console.log(err);
+    }
+        document.getElementById('txtinp').value= ""; //clear the cmnd line after each exec
+}
+//dark mode switch click func.
 const swtch = (arg) => {
     if (arg === 0) {
         automate1(arg);
@@ -110,6 +129,21 @@ const swtch = (arg) => {
     }
     
 }
+
+
+//ALL ACTIONS:
+
 document.getElementsByClassName('dark')[0].addEventListener("click", function(){
     swtch(drk)
 });
+document.getElementsByClassName("bttn")[0].addEventListener("click", function(){
+    cnsole_final()
+})
+document.getElementsByClassName("txtinp")[0].addEventListener("keyup", function(event) { //foe Enter Button
+    if (event.keyCode === 13) {
+     event.preventDefault();
+     document.getElementsByClassName("bttn")[0].click();
+    }
+})
+// When the user scrolls the page, execute headerFixed
+window.onscroll = () => {headerFixed()};
