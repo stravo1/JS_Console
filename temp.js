b5dcda19f20000503651ac18fddcd300d7fe5d90
Stravo1 <stravoone@gmail.com>
2021-01-02 00:44:13 +0530

    Added objLogger + transparency for abt and header

--- temp.js
+++ temp.js
@@ -1,9 +1,29 @@
+const objLogger = (arg) => {
+  var keys = Object.getOwnPropertyNames(arg);
+  var out = 'Object {'
+  for (var i = 0; i < keys.length; i++) {
+    out += keys[i]+":";
+    (typeof(arg[keys[i]]) != 'object')&&(typeof(arg[keys[i]]) != 'function') ? out+=JSON.stringify(arg[keys[i]]) : typeof(arg[keys[i]]) == 'function' ? out += 'f' : out += '{...}';
+      i != keys.length-1 ? out+= ", " : out+=''
+  }
+  out += "}"
+  return out
+} //for console.logs of objects
+
+
+
+
 const automate = (...args) => {
-    let out = ""
-    for (var i = 0; i < args.length; i++) {
-        out+= " "+args[i]
+    let outp = ""
+    
+    for (var j = 0; j < args.length; j++) { //stackovrflw
+      if (typeof args[j] == 'object') {
+        outp += objLogger(args[j]);
+    } else {
+      outp += " " + args[j]
     }
-    document.getElementById('console').innerHTML+="<- "+out+"<br>"
+    document.getElementById('console').innerHTML+="<- "+outp+"<br>"
+    }
 }
 // define a new console
 var consolen=(function(oldCons){ //*n
@@ -70,7 +90,7 @@ const automate0 = (arg) => { //add spans with color depending upon drk state (wa
 
 //automate changing the classes to 'l' or 'd'
 const automate1 = (arg) => {
-    var id = ['h1','dark','readonly','txtinp','bttn','info','abt','txt','warn'];
+    var id = ['h1','dark','readonly','txtinp','bttn','info','abt','txt','warn','header'];
     var state = ['l','d'];
     for (let i = 0; i < id.length; i++) {
             document.getElementById(id[i]).setAttribute('class',id[i]+" "+state[arg===0 ? 1 : 0]);
@@ -139,6 +159,7 @@ var infs = 0; //initial info state
 const infoBttn = (inf) => { //some cleanup required
   state= ['35%','0%'];
   if (inf !== 'click') {
+      document.getElementById('abt').style.setProperty('opacity','0.8')
       document.getElementsByClassName('abt')[0].style.setProperty('height',state[inf]);
       document.getElementsByClassName('abt')[0].style.setProperty('width',state[inf]);
       document.getElementsByClassName('abt')[0].style.setProperty('padding',inf === 0 ? '2px 10px' : '0px 0px');
@@ -147,6 +168,7 @@ const infoBttn = (inf) => { //some cleanup required
       document.querySelector('#abt').scrollTop = 0;
   } else {
     infs = 0;
+    document.getElementById('abt').style.setProperty('opacity','1')
   }
 }
 
